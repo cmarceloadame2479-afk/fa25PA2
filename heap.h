82ed612f14015cb87d409ed480fdc0613ab612f1
Cristofer MarceloAdame <cmarceloadame2479@sdsu.edu>
2025-11-03 16:25:30 -0800

    Completed the push() and pop() methods in the heap.h. push() should add a new index at the end of the array and upheap to its rightful spot and pop() replaces the root with the last element and returns the smallest index.

--- heap.h
+++ heap.h
@@ -16,16 +16,36 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
+        if (size >= 64) {
+            cout << "Error" << endl;
+            return;
+        }
+
+        // Inserts new element at the end of the array
+        data[size] = idx;
+        // Restore heap order
+        upheap(size, weightArr);
+        size++;
     }
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        if (size == 0) {
+            cout << "Error" << endl;
+            return -1;
+        }
+        int minIdx = data[0];
+        size--;
+        data[0] = data[size];
+        downheap(0, weightArr);
+        return minIdx; // placeholder
     }
 
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
+
+
     }
 
     void downheap(int pos, int weightArr[]) {
